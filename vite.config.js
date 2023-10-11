e117a569bbf85a13302a08de31d658bd8890d34e
Anita Stasiełuk <a.stasieluk99@gmail.com>
2023-10-11 14:51:29 +0200

    change config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/tower_game/",
     plugins: [
         react({
             include: "**/*.jsx",
